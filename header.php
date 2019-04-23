536acf9836bcd10a768d263d6a46952086351f1d
ehking <ehsanbagban@gmail.com>
2019-04-23 08:29:23 +0430

    add file,header change

--- header.php
+++ header.php
@@ -28,16 +28,46 @@
     <?php
     }
     ?>
+    <div class="col-sm-12 header <?php if ( is_admin_bar_showing() ) {echo "margin_top_55";}?>">
+        <div class="container">
+             <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <a href="#" id="headingimg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/NastaliqOnline.png" alt=""></a>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="row">
+                        <div class="search-header">
+                            <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+                                <label>
+                                    <input type="search" class="search-field" placeholder=" جست و جوی شخصیت ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="جست و جوی شخصیت ..." />
+                                </label>
+                                <input type="submit" class="search-submit btn btn-success" value="جست و جو" />
+                                <select name="food_selector" data-placeholder="همه دسته ها">
+                                    <option value="AnyCategory">همه دسته ها</option>
+                                <?php
 
-<div class="col-sm-12 header <?php if ( is_admin_bar_showing() ) {echo "margin_top_55";}?>">
-    <div class="row">
-        <div class="col-sm-6">
-            <a href="#" id="headingimg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/NastaliqOnline.png" alt=""></a>
+                                $categories = get_categories();
+                                foreach($categories as $category) {
+                                    echo '<option value="' . get_category_link($category->term_id) . '">' . $category->name . '</option>';
+                                }
+                                ?>
+                                </select>
+                            </form>
+                        </div>
+                     </div>
+                </div>
+             </div>
         </div>
-        <div class="col-sm-6">
+        <div class="row">
+            <div class="col-sm-12 slider">
 
+            </div>
         </div>
     </div>
 </div>
-</div>
-<!--<script src="--><?php //echo get_stylesheet_directory_uri(); ?><!--/js/app.js"></script>-->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js"></script>
+
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.bundle.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/select.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/app.js"></script>
