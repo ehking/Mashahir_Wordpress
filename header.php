614e813e861171143e0bc6ea87618a91652a9161
Ehsan Baghbanan <ehsanbagbanan@gmail.com>
2019-04-24 15:30:04 +0430

    slider create

--- header.php
+++ header.php
@@ -6,6 +6,13 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/ico/favicon-16x16.png">
     <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/ico/site.webmanifest">
     <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/ico/safari-pinned-tab.svg" color="#5bbad5">
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js"></script>
+
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.bundle.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/select.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/until.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/app.js"></script>
     <meta name="msapplication-TileColor" content="#2d89ef">
     <meta name="theme-color" content="#ffffff">
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -15,6 +22,7 @@
     <?php wp_head(); ?>
 </head>
 <body>
+
 <div class="row">
 
     <?php
@@ -66,74 +74,50 @@
 <div class="row">
     <div class="container">
         <div class="col-sm-12 slider">
-            <img class="img-fluid" src="<?php echo get_option('logo');  ?>" alt="header"">
+            <?php putRevSlider("Homepage") ;?>
         </div>
-    <div class="col-sm-12 menu">
-<!--        <ul class="nav nav-tabs" id="myTab" role="tablist">-->
-<!---->
-<!--            --><?php
-//            foreach($categories as $category) {
-//                if ($category->parent == "") {
-//                    echo '<li class="nav-item col-lg-2 col-md-3 col-sm-6">';
-//                    echo '<a class="nav-link" id="'. $category->slug.'-tab" data-toggle="tab" href="#'. $category->slug.'" role="tab" aria-selected="false" aria-controls="'. $category->term_id .'" >' ;
-//                    echo '<i class="icon fa '.$category->term_font_icon.'"></i>'. $category->name.'</a>';
-//                    echo ' </li>';
-//                }
-//            }
-//            ?>
-<!--        </ul>-->
-<!--            --><?php
-//            foreach($categories as $category) {
-//                if ($category->parent == "") {
-//                    $categoryid=$category->term_id;
-//                  $msg='<div class="col-sm-12 sub-menu">';
-//                   $msg.= '<ul>';
-//                    foreach($categories as $categoryc) {
-//                        if ($categoryid == $categoryc->parent) {
-//                            $msg.= '<li> '.$categoryc->name.'</li>';
-//                        }
-//                    }
-//            $msg.='</ul>';
-//            $msg.='</div>';
-//                 echo '<div class="tab-pane deactive fade" id="'.$category->slug.'" role="tabpanel" aria-labelledby="'.$category->slug.'-tab">
-//                 '.$msg.'
-//                 </div>';
-//                }
-//            }
-//            ?>
+        <div class="col-sm-12 menu">
 
-        <ul id="main_menu">
+
+            <ul id="main_menu">
+            <div class="row">
                  <?php
+                 $n=1;
             foreach($categories as $category) {
-                if ($category->parent == "") {
-                    echo '<li class="main_list"><a href="">'. $category->name.'<i class="fa '.$category->term_font.'"></i></a>';
-                    echo ' <ul class="animate fadeInDown">
-                    <ol>
-                        <a class="mmmk" href="">
-                            <li>'. $category->name.'</li>
-                        </a>
-                    </ol>
-                </ul>';
-                    echo '</li>';
+
+                if (in_array($category->term_id,get_option('cat'))) {
+                    $categoryid=$category->term_id;
+                    echo '<li class="main_list"><a href=""><i class="fa '.$category->term_font_icon.'"></i>'. $category->name.' </a>';
+                    echo '<ul class="animate fadeInDown">';
+                    echo '<ol>';
+                    foreach ($categories as $categoryc){
+                        if ($n==4){
+                            echo "</ol><ol>";
+                            $n=0;
+                        }
+                        if ($categoryid == $categoryc->parent) {
+                            echo '<a class="" href="">
+                            <li>'. $categoryc->name.'</li>
+                        </a>';
+                            $n++;
+                        }
+                    }
+                echo '</ul></li>';
                 }
             }
-            ?>
-        </ul>
-        <br>
-        <p>rf</p>
 
-    </div>
+            ?>
+            </div>
+        </ul>
+
+         </div>
+
     </div>
 </div>
 
 
-
-
-
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js"></script>
-
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.bundle.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/select.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/until.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/app.js"></script>
+<div class="row">
+    <div class="col-sm-12">
+        <img class="img-fluid" src="<?php echo get_option('logo');  ?>" alt="header"">
+    </div>
+</div>
